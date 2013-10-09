2e48ad553f24c68b77bd07df267b0fe28317d777
Dan Walsh <dwalsh@redhat.com>
2013-10-09 16:18:15 -0400

    Add selinux_current_policy_path, which returns the a pointer to the loaded policy
    Also change audit2why to look at the loaded policy rather then searching on disk for
    the policy file.  It is more likely that you are examining the running policy.

--- libselinux/src/selinux_config.c
+++ libselinux/src/selinux_config.c
@@ -350,17 +350,20 @@ const char *selinux_current_policy_path(void)
 	int vers = 0;
 	static char policy_path[PATH_MAX];
 
-	snprintf(policy_path, sizeof(policy_path), "%s/policy", selinux_mnt);
-	if (access(policy_path, F_OK) == 0) {
-		vers = security_policyvers();
-		do {
-			/* Check prior versions to see if old policy is available */
-			snprintf(policy_path, sizeof(policy_path), "%s.%d",
-				selinux_binary_policy_path(), vers);
-		} while ((rc = access(policy_path, F_OK)) && --vers > 0);
-
-		if (rc) return NULL;
+	if (selinux_mnt) {
+		snprintf(policy_path, sizeof(policy_path), "%s/policy", selinux_mnt);
+		if (access(policy_path, F_OK) == 0 ) {
+			return policy_path;
+		}
 	}
+	vers = security_policyvers();
+	do {
+		/* Check prior versions to see if old policy is available */
+		snprintf(policy_path, sizeof(policy_path), "%s.%d",
+			 selinux_binary_policy_path(), vers);
+	} while ((rc = access(policy_path, F_OK)) && --vers > 0);
+	
+	if (rc) return NULL;
 	return policy_path;
 }
 
